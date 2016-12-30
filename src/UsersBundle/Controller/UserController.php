e407e839178dfc0db885f148ca0c04b280610849
Franpastoragusti <franciscopa91@gmail.com>
2016-12-30 21:12:52 +0100

    remember me aplicado

--- src/UsersBundle/Controller/UserController.php
+++ src/UsersBundle/Controller/UserController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Cookie;
 use UsersBundle\Entity\User;
 use UsersBundle\Form\UserType;
 
